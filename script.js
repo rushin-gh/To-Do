abbea4c926555c4c6b0e0d19753f494adbcdf0ff
rushin-gh <rushin.mailme@gmail.com>
2025-06-10 01:02:36 +0530

    Worked with Enter event rather than add button

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 let toDoList = [];
 
-let todoAddBtn = document.getElementById('addToDo');
 let inputToDo = document.getElementById('toDoInput');
 let displayToDo = document.getElementById('displayToDo');
 
-todoAddBtn.addEventListener('click', function() {
-    let todo = inputToDo.value;
-    if (todo) {
-        toDoList.push(todo);
-        
-        let toDoListToShow = '';
-        for (let todo of toDoList) {
-            toDoListToShow += (`<li>${todo}</li>`);
+inputToDo.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        let todo = inputToDo.value;
+        if (todo) {
+            toDoList.push(todo);
+            
+            let toDoListToShow = '';
+            for (let todo of toDoList) {
+                toDoListToShow += (`<li>${todo}</li>`);
+            }
+    
+            displayToDo.innerHTML = toDoListToShow;
+        } else {
+            alert('Please enter non empty To Do.')
         }
-
-        displayToDo.innerHTML = toDoListToShow;
-    } else {
-        alert('Please enter non empty To Do.')
+        inputToDo.value = '';
     }
 });
