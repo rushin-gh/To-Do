c1ee49d6cb4f0f48661511623fa92893580ddefe
rushin-gh <rushin.mailme@gmail.com>
2025-06-11 00:57:33 +0530

    Add delete functionality to delete To Do from List

--- script.js
+++ script.js
@@ -2,22 +2,41 @@ let toDoList = [];
 
 let inputToDo = document.getElementById('toDoInput');
 let displayToDo = document.getElementById('displayToDo');
+let deleteToDo = document.getElementsByClassName('deleteToDo');
 
 inputToDo.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         let todo = inputToDo.value;
         if (todo) {
             toDoList.push(todo);
-            
-            let toDoListToShow = '';
-            for (let todo of toDoList) {
-                toDoListToShow += (`<li>${todo}</li>`);
-            }
-    
-            displayToDo.innerHTML = toDoListToShow;
+            DisplayToDo();
         } else {
             alert('Please enter non empty To Do.')
         }
         inputToDo.value = '';
     }
 });
+
+displayToDo.addEventListener('click', function(event) {
+    if (event.target.classList.contains('delToDo')) {
+        let id = event.target.id;
+        let toDoIdx = id.split('-')[1];
+        toDoList.splice(toDoIdx, 1);
+        DisplayToDo();
+    }
+});
+
+function DisplayToDo () {
+    let toDoListToShow = '';
+    for (let i = 0; i < toDoList.length; i++) {
+        toDoListToShow += 
+        `<tr>
+            <td>${i + 1}</td>
+            <td>${toDoList[i]}</td>
+            <td></td>
+            <td class="delToDo" id="delToDo-${i}"></td>
+        </tr>`;
+    }
+
+    displayToDo.innerHTML = toDoListToShow;
+}
